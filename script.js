78cd8e76cd17a90b3d6bee8c17d5008222f85e15
Levente27 <levomen07@gmail.com>
2025-09-27 14:54:17 +0200

    képek hozzáadása az img mappához, scriptben intervallum létrehozása, htmlben kepvetítés megjelenítése

---
+++ script.js
@@ -0,0 +1,18 @@
+const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","Megjött a friss GDP-jelentés.jpg"]
+const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé – rajtunk múlik.”"]
+
+let kepkeres = document.getElementById("kepvetites")
+let mottokeres = document.getElementById("kepvetitesmotto")
+
+let i = 0
+setInterval(() => {
+    
+    kepkeres.innerHTML = `<img src="img/${kepek[i]}" alt="${mottok[i]}" class="mozgokepek">`
+    mottokeres.innerHTML = mottok[i]
+
+    if (i == kepek.length)
+    {
+        i = 0
+    }
+
+}, 3500);
