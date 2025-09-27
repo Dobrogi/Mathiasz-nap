9fd5e168e4ae921c573445dc5fccc809b64ed14f
Levente27 <levomen07@gmail.com>
2025-09-27 15:06:58 +0200

    mottok helyének beillsztése és script modósítás

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","Megjött a friss GDP-jelentés.jpg"]
-const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé – rajtunk múlik.”"]
+const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé  rajtunk múlik.”"]
 
 let kepkeres = document.getElementById("kepvetites")
-let mottokeres = document.getElementById("kepvetitesmotto")
+let mottokeres = document.getElementById("motto")
 
 let i = 0
 setInterval(() => {
     
     kepkeres.innerHTML = `<img src="img/${kepek[i]}" alt="${mottok[i]}" class="mozgokepek">`
-    mottokeres.innerHTML = mottok[i]
+    mottokeres.innerHTML = `<p>${mottok[i]}</p>`
     
     i++
 
