1994d7cc01f3ebc5d0515d5c9ae899b61febf5ad
Levente27 <levomen07@gmail.com>
2025-09-28 11:01:47 +0200

    kép áttűnések megvalósítása, kép átnevezés

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","Megjött a friss GDP-jelentés.jpg"]
-const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé  rajtunk múlik.”"]
+const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","megjott_gdp_jelentes.jpg"]
+
+const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé rajtunk múlik.”"]
 
 let kepkeres = document.getElementById("kepvetites")
 let mottokeres = document.getElementById("motto")
 
 let i = 0
-setInterval(() => {
-    
-    kepkeres.innerHTML = `<img src="img/${kepek[i]}" alt="${mottok[i]}" class="mozgokepek">`
-    mottokeres.innerHTML = `<p>${mottok[i]}</p>`
-    
-    i++
 
-    if (i == kepek.length)
-    {
-        i = 0
+
+function kepBetolt(index) {
+    const img = document.createElement("img")
+    img.src = `img/${kepek[index]}`
+    img.alt = mottok[index]
+    img.classList.add("mozgokepek")
+
+    kepkeres.appendChild(img)
+
+    setTimeout(() => {
+        img.classList.add("megjelenik")
+    }, 50)
+
+    mottokeres.innerHTML = `<p>${mottok[index]}</p>`
+}
+
+kepBetolt(i)
+
+setInterval(() => {
+    const aktualisKep = kepkeres.querySelector("img")
+    if (aktualisKep) {
+
+        aktualisKep.classList.remove("megjelenik")
+        aktualisKep.classList.add("kifakul")
+
+        setTimeout(() => {
+            if (aktualisKep && aktualisKep.parentNode) {
+                kepkeres.removeChild(aktualisKep)
+            }
+
+            i = (i + 1) % kepek.length
+            kepBetolt(i)
+
+        }, 1000) 
     }
 
-}, 3500);
+}, 3500)
