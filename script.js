485ce6d45a70552b7fde1cae28c44d9b3018781b
Melasz <kalasz.laszlo@liveedu.mathiasz.hu>
2025-10-02 10:58:15 +0200

    linearis quiz tablageneralo megkezdese

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","megjott_gdp_jelentes.jpg"]
+/*const kepek = ["penz.jpg","piggy-bank-850607_1280.jpg","megjott_gdp_jelentes.jpg"]
 
 const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spórolsz, holnap mosolyogsz.”","„Gazdaság: felfelé vagy lefelé rajtunk múlik.”"]
 
 let kepkeres = document.getElementById("kepvetites")
-let mottokeres = document.getElementById("motto")
+let mottokeres = document.getElementById("motto")*/
 
 let i = 0
 
@@ -12,13 +12,13 @@ const carouselMotto = document.querySelectorAll("#motto p")
 
 setInterval(() => {
     const elozo = i
-    i = (i+1) % carouselKepek.length
+    i = (i + 1) % carouselKepek.length
     carouselKepek[elozo].classList.remove("aktiv")
     carouselMotto[elozo].classList.remove("aktiv")
-    
+
     carouselKepek[elozo].classList.add("after")
     carouselMotto[elozo].classList.add("after")
-    
+
     carouselKepek[i].classList.add("aktiv")
     carouselMotto[i].classList.add("aktiv")
     setTimeout(() => {
@@ -28,46 +28,12 @@ setInterval(() => {
 }, 3000);
 
 const osszesMain = document.querySelectorAll("body main")
-function navMenu(indeksz){
+function navMenu(indeksz) {
     osszesMain.forEach(elem => elem.classList.remove("aktiv"))
     osszesMain[indeksz].classList.add("aktiv")
 }
-
-
-/*
-function kepBetolt(index) {
-    const img = document.createElement("img")
-    img.src = `img/${kepek[index]}`
-    img.alt = mottok[index]
-    img.classList.add("mozgokepek")
-
-    kepkeres.appendChild(img)
-
-    setTimeout(() => {
-        img.classList.add("megjelenik")
-    }, 50)
-
-    mottokeres.innerHTML = `<p>${mottok[index]}</p>`
+function kiir(){
+    document.getElementById("kiiros").innerText = `${document.getElementById("evsz").value} év`
 }
 
-kepBetolt(i)
 
-setInterval(() => {
-    const aktualisKep = kepkeres.querySelector("img")
-    if (aktualisKep) {
-
-        aktualisKep.classList.remove("megjelenik")
-        aktualisKep.classList.add("kifakul")
-
-        setTimeout(() => {
-            if (aktualisKep && aktualisKep.parentNode) {
-                kepkeres.removeChild(aktualisKep)
-            }
-
-            i = (i + 1) % kepek.length
-            kepBetolt(i)
-
-        }, 1000) 
-    }
-
-}, 3500)*/
