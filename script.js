0cd659bf073159681555d019418793660f288229
Melasz <kalasz.laszlo@liveedu.mathiasz.hu>
2025-10-04 18:06:42 +0200

    tábla generáló kész, egyszerűbb generálás

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const mottok = ["„Minden kis lépés közelebb visz a célhoz.”","„Ma spó
 
 let kepkeres = document.getElementById("kepvetites")
 let mottokeres = document.getElementById("motto")*/
-/*
+
 let i = 0
 const carouselKepek = document.querySelectorAll("#kepvetites img")
 const carouselMotto = document.querySelectorAll("#motto p")
-
+/*
 setInterval(() => {
     const elozo = i
     i = (i + 1) % carouselKepek.length
@@ -36,7 +36,7 @@ function kiir() {
     document.getElementById("kiiros").innerText = `${document.getElementById("evsz").value} év`
 }
 function kezdoErtekLimit(elem, azon) {
- 
+
     let mezok = document.querySelectorAll("div input[name=kezdoAdat]:checked")
     let inputok = document.querySelectorAll("section.ertekAdo aside:not(.marad) input")
     let asideok = document.querySelectorAll("section.ertekAdo aside:not(.marad)")
@@ -46,7 +46,7 @@ function kezdoErtekLimit(elem, azon) {
     mezok.forEach(e => {
         if (e.checked) hossz++
     });
-    if (hossz > 2) {
+    if (hossz > 1) {
         elem.checked = false;
         inputok[azon].disabled = true
         alert("Legfeljebb csak kettő kezdőérték lehet! Ugye nem akarsz kész feladatot csinálni?")
@@ -65,16 +65,54 @@ function kezdoErtekLimit(elem, azon) {
         asideok[azon].classList.remove("enabled")
     }
 }
-function tablaGeneralas(){
-    let tablaSzulo = document.getElementById("tablaSzulo")
-    let ev = document.getElementById("evsz")
-        tablaSzulo.innerHTML = <table id="linTabla"></table>
-    let i = 0
-    for (i = 0; i < ev.value; i++) {
-        tablaSzulo.innerHTML = `<tr id="tablaRow${i}"></tr>`
-        let tablaRow = document.getElementById("tablaRow${i}")
-        for (let j = 0; j < 4; j++) {
-            tablaRow.innerHTML = `<td id="tableData${i};${j}"></td>`
+function tablaGeneralas() {
+    let tablaSzulo = document.getElementById("linTablaSzulo")
+    let ev = document.getElementById("linEvsz")
+    let maradvany = document.getElementById("linMaradvany")
+    let brutto = document.getElementById("linBrutto").value
+    let ecs = document.getElementById("linEcs").value
+    let tartalom = '<table id="linTabla" class="arnyek">'
+
+    if (brutto == "" || ecs == "")
+        alert("Számok nélkül nehéz számolni, tölts ki minden mezőt!")
+    else {
+        let i = 0
+        let j = 0
+        for (i = 0; i < (parseInt(ev.value) + 1); i++) {
+
+
+            tartalom += `<tr id="linTableRow${i}">`
+
+            for (j = 0; j < 5; j++) {
+
+                if (i == 0) {
+                    tartalom += `<td id="linTableData${i}_${j}" class="linTableHeader">`
+                    switch (j) {
+                        case 0: tartalom += "Évek"; break;
+                        case 1: tartalom += "Bruttó"; break;
+                        case 2: tartalom += "Értékcsökkenés"; break;
+                        case 3: tartalom += "Halmozott Écs"; break;
+                        case 4: tartalom += "Nettó"; break;
+                    }
+                    tartalom += `</td>`
+                }
+                else if (i == parseInt(ev.value) && j == 4 && (maradvany.value != 0 || maradvany.value != "")) {
+
+                    tartalom += `<td id="linTableData${i}_${j}" class="linTablaMaradvany">${parseInt(maradvany.value)}</td>`
+                }
+                else {
+                    switch (j) {
+                        case 0: tartalom += `<td id="linTableData${i}_${j}" class="linEvek">${i}`; break;
+                        case 1: tartalom += `<td id="linTableData${i}_${j}" class="defaultValue">${brutto}`; break;
+                        case 2: tartalom += `<td id="linTableData${i}_${j}" class="defaultValue">${ecs}`; break;
+                        default: tartalom += `<td id="linTableData${i}_${j}" contenteditable>`; break;
+                    }
+                    tartalom += "</td>"
+                }
+            }
+            tartalom += "</tr>"
         }
+        tartalom += "</table>"
+        tablaSzulo.innerHTML = tartalom
     }
 }
